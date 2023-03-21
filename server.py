e594f8332542a654ddaff6dfc4e1c77917403ba1
Katy Smith <catriona.f.smith@gmail.com>
2023-03-21 11:22:52 +1100

    Add HTML5 UP design and image files

--- server.py
+++ server.py
@@ -3,12 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def make_index():
     return render_template('index.html')
-
-@app.route('/cv')
-def get_cv():
-    return render_template('cv.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
